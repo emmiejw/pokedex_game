04b5ff02cbd66ba9720461dee1d611849d5159bf
emmiejw <emilyprice84@outlook.com>
2019-06-16 19:52:29 +0100

    pokedex game using poke_api with react.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Pokegame from './pokegame';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+        <img className="logo" src="http://pluspng.com/img-png/pokemon-logo-png-pokemon-logo-png-2000.png" height="150px" />
+          <Pokegame />
+      </div>
+      );
+    }
 }
 
 export default App;
